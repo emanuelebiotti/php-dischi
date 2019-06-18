4bdc18ca0bfbac71da041ab1dc07d5cd02959681
emanuele <emanuele.biotti@gmail.com>
2019-06-18 18:57:26 +0200

    impostazione ambiente di lavoro

---
+++ data.php
@@ -0,0 +1,42 @@
+<?php
+
+$dischi = [
+
+  $disco1 => [
+   'immagine_copertina' => '',
+   'titolo_album' => '',
+   'nome_artista' => '',
+   'anno_uscita' => ''
+  ],
+
+  $disco1 => [
+   'immagine_copertina' => '',
+   'titolo_album' => '',
+   'nome_artista' => '',
+   'anno_uscita' => ''
+  ],
+
+  $disco1 => [
+   'immagine_copertina' => '',
+   'titolo_album' => '',
+   'nome_artista' => '',
+   'anno_uscita' => ''
+  ],
+
+  $disco1 => [
+   'immagine_copertina' => '',
+   'titolo_album' => '',
+   'nome_artista' => '',
+   'anno_uscita' => ''
+  ],
+
+  $disco1 => [
+   'immagine_copertina' => '',
+   'titolo_album' => '',
+   'nome_artista' => '',
+   'anno_uscita' => ''
+  ],
+
+]
+
+ ?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include data.php;
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="public/css/app.css">
+  </head>
+  <body>
+
+    <h1>Ecco un po' di album musicali </h1>
+
+    <div class="main">
+
+      <h2>questo è un disco:</h2>
+      <div class="disco">
+        <?php
+
+
+        ?>
+
+      </div>
+    </div>
+
+
+
+  </body>
+</html>
